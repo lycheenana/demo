63a23fdb1f55a76152019ffe531ad7508ada90dc
my_wang <my_wang@kingdee.com>
2021-09-17 14:45:44 +0800

    feature: lint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,5 +1,22 @@
 module.exports = {
-    rules: {
-        'type-enum': [2, 'always', ['feature']]
-    }
-}
+  rules: {
+    "body-leading-blank": [1, "always"],
+    "footer-leading-blank": [1, "always"],
+    "header-max-length": [2, "always", 360],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      ["fix", "feature", "style", "test", "revert", "build"],
+    ],
+  },
+};
